1c6e37c6b5d3162e612d496f89a7358da0a122ea
adityananaware2004 <aditya.nanaware.2004@gemail.com>
2025-04-25 17:03:02 +0530

    todays work done

--- BACKEND/router/userRouter.js
+++ BACKEND/router/userRouter.js
@@ -1,9 +1,21 @@
 import express from 'express';
-import { patientRegister, login ,addNewAdmin } from '../controller/userController.js';
-
+import { patientRegister, login ,addNewAdmin , getAllDoctors , addNewDoctor, getUserDetails , logoutPatient, logoutAdmin } from '../controller/userController.js';
+import {
+    isAdminAuthenticated,
+    isPatientAuthenticated,
+  } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/patient/register", patientRegister); // Correct function name
 router.post("/login", login);
-router.post("/admin/addnew",  addNewAdmin);
+router.post("/admin/addnew", isAdminAuthenticated,  addNewAdmin);
+router.get("/doctors", getAllDoctors);
+router.get("/patient/me", isPatientAuthenticated, getUserDetails);
+router.get("/admin/me", isAdminAuthenticated, getUserDetails);
+router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
+router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
+router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
+
+
+
 export const userRouter = router;
